4e4ddd2b136480d979b7fabcf53786c2f6590992
Mistweaver <bryce.a.egley@gmail.com>
2018-11-03 12:04:14 -0700

    Pacman added.  Save point before major design change

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-    console.log("ready");
-    $("#fadeBox").on(
-      "animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
-      function() {
-        // $(this).addClass("opaque");
-        var elem = document.querySelector('#fadeBox');
-        elem.style.display = 'none';
-        var elem = document.querySelector('#quoteBox');
-        elem.parentNode.removeChild(elem);
-      }
-  );
+	console.log("ready");
+	$("#fadeBox").on(
+		"animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+	  	function() {
+			// $(this).addClass("opaque");
+			var elem = document.querySelector('#fadeBox');
+			elem.style.display = 'none';
+			var navbar = document.querySelector('.fixedSideBarNav');
+			navbar.style.display = 'flex';
+			var elem = document.querySelector('#quoteBox');
+			elem.parentNode.removeChild(elem);
+	  	}
+	);	  
 });
